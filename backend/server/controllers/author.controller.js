9055ce90a0154fd3618c3063d29f942255acb5ef
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-28 10:53:40 -0600

    added backend

---
+++ backend/server/controllers/author.controller.js
@@ -0,0 +1,31 @@
+const Author = require("../models/author.model");
+
+module.exports.findAllAuthors = (req, res) => {
+    Author.find()
+        .then(allDaAuthors => res.json(allDaAuthors))
+        .catch(err => res.json({ message: "Something went wrong",  err }));
+};
+
+module.exports.findOneSingleAuthor = (req, res) => {
+    Author.findOne({ _id: req.params._id })
+        .then(oneSingleAuthor => res.json({ author: oneSingleAuthor }))
+        .catch(err => res.json({ message: "Something went wrong",  err }));
+};
+
+module.exports.createNewAuthor = (req, res) => {
+    Author.create(req.body)
+        .then(newlyCreatedAuthor => res.json({ author: newlyCreatedAuthor }))
+        .catch(err => res.json({ message: "Something went wrong",  err }));
+};
+
+module.exports.updateExistingAuthor = (req, res) => {
+    Author.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true })
+        .then(updatedAuthor => res.json({ author: updatedAuthor }))
+        .catch(err => res.json({ message: "Something went wrong",  err }));
+};
+
+module.exports.deleteAnExistingAuthor = (req, res) => {
+    Author.deleteOne({ _id: req.params._id })
+        .then(result => res.json({ result: result }))
+        .catch(err => res.json({ message: "Something went wrong",  err }));
+};
---
+++ backend/server/models/author.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const AuthorManagerSchema = new mongoose.Schema(
+	{
+		author: {
+			type: String,
+			required: [true, "Author must be 3 Characters Long "],
+			minLength: [3, "Author must be 3 Characters Long "]
+		},
+
+
+	},
+	{ timestamps: true }
+);
+
+const Authors = mongoose.model("Authors", AuthorManagerSchema);
+
+module.exports = Authors;
